fd26c575bbd3d9dc0610927fbc86f5c8aef3c3b7
Madha Arif <madhaarif9@gmail.com>
2026-03-10 07:14:59 +0500

    feat: add meta tags for SEO

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -12,6 +12,18 @@ const SEO = ({ pageTitle, font }) => (
       name='viewport'
       content='width=device-width, initial-scale=1, shrink-to-fit=no'
     />
+    <meta name="description" content="PACT is a professional IT skills college in Pakistan that offers innovative learning and training in modern technologies. We provide the best IT education and courses to help you succeed in your career." />
+    <meta name="keywords" content="PACT, Professional IT Skills College, IT education, IT training, innovative learning, modern technologies, IT courses, Pakistan, professional skills" />
+    <meta name="author" content="PACT" />
+    <meta property="og:title" content="PACT - Professional IT Skills College" />
+    <meta property="og:description" content="PACT is a professional IT skills college in Pakistan that offers innovative learning and training in modern technologies. We provide the best IT education and courses to help you succeed in your career." />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://www.pact.edu.pk/" />
+    <meta property="og:image" content="https://www.pact.edu.pk/logo-white.svg" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="PACT - Professional IT Skills College" />
+    <meta name="twitter:description" content="PACT is a professional IT skills college in Pakistan that offers innovative learning and training in modern technologies. We provide the best IT education and courses to help you succeed in your career." />
+    <meta name="twitter:image" content="https://www.pact.edu.pk/logo-white.svg" />
     {font && <link href={font} rel='stylesheet' />}
     <link rel='icon' href='/logo-white.svg' />
   </Head>
