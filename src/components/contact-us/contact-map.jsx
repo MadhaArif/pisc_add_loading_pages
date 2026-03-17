cc2c4a7de6a93a0af9abf64206835add2534f03c
Asad Ali <46473237+Axadali@users.noreply.github.com>
2026-03-17 13:56:52 +0500

    Change Google Map coordinates in contact-map.jsx
    Updated the map location coordinates in the iframe.

--- src/components/contact-us/contact-map.jsx
+++ src/components/contact-us/contact-map.jsx
@@ -6,14 +6,14 @@ const ContactMap = () => {
       <div className="mapouter">
         <div className="gmap_canvas">
           <iframe
-            title="Google Map"
-            src="https://www.google.com/maps?q=31.601389,74.335924&output=embed"
-            width="100%"
-            height="450"
-            style={{ border: 0 }}
-            loading="lazy"
-            allowFullScreen
-          ></iframe>
+  title="Google Map"
+  src="https://www.google.com/maps?q=31.598031,74.3433967&output=embed"
+  width="100%"
+  height="450"
+  style="border:0;"
+  loading="lazy"
+  allowfullscreen
+></iframe>
         </div>
       </div>
     </div>
